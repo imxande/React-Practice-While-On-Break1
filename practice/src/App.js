8bc7103f67873bbb53c6005af850fed739397212
imxande <alerodriguezlarosa@gmail.com>
2019-12-23 17:39:17 -0500

    practice onClick and useState for scoreboard

---
+++ practice/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ScoreBoard} from './ScoreBoard';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ScoreBoard />
+    </div>
+  );
+}
+
+export default App;
